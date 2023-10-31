66c0538e1ea58168b90ad7d15c3e6a6454a4d95c
farzad kh <farzadkh900@gmail.com>
2023-10-31 19:33:42 +0100

    clear log

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -22,15 +22,6 @@ const LineChart = ({ coinHistory, currentPrice, coinName, isFetching }) => {
 
     const eachTimestamp = coinHistory?.data?.history?.map(item => new Date(item.timestamp * 1000).toLocaleDateString())
 
-    // for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
-    //     coinTimestamp.push(coinHistory?.data?.history[i].timestamp * 1000);
-    // }
-    // for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
-    //     coinPrices.push(coinHistory?.data?.history[i].price);
-    //   }
-
-    console.log(coinPrice);
-
 
 
     const data = {
@@ -70,9 +61,8 @@ const LineChart = ({ coinHistory, currentPrice, coinName, isFetching }) => {
 
     }, [coinHistory])
 
-    // if (!isFetching) return <ChartLoad/>
-    console.log(coinPrice)
-    console.log(coinTimestamp)
+
+
     if (isFetching) return <ChartLoad />
 
 
@@ -110,66 +100,3 @@ export default LineChart;
 
 
 
-
-// import React, { useEffect, useState } from 'react';
-// import Chart from 'chart.js/auto';
-// import { Line } from 'react-chartjs-2';
-// import { Col, Row, Typography } from 'antd';
-// import Loading from './Loading';
-
-// const { Title } = Typography;
-
-// const LineChart = ({ coinHistory, currentPrice, coinName, coinHistoryFeching, isFetching }) => {
-//     if (coinHistory?.data?.history == undefined) return <Loading text="Please wait" />
-//     const coinPrice = [];
-//   const coinTimestamp = [];
-
-//   for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
-//     coinPrice.push(coinHistory?.data?.history[i].price);
-//   }
-
-//   for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
-//     coinTimestamp.push(new Date(coinHistory?.data?.history[i].timestamp*1000).toLocaleDateString());
-//   }
-//   const data = {
-//     labels: coinTimestamp,
-//     datasets: [
-//       {
-//         label: 'Price In USD',
-//         data: coinPrice,
-//         fill: false,
-//         backgroundColor: '#0071bd',
-//         borderColor: '#0071bd',
-//       },
-//     ],
-//   };
-
-//   const options = {
-//     scales: {
-//       yAxes: [
-//         {
-//           ticks: {
-//             beginAtZero: true,
-//           },
-//         },
-//       ],
-//     },
-//   };
-
-//   return (
-//     <>
-//       <Row className="chart-header">
-//         <Title level={2} className="chart-title">{coinName} Price Chart </Title>
-//         <Col className="price-container">
-//           <Title level={5} className="price-change">Change: {coinHistory?.data?.change}%</Title>
-//           <Title level={5} className="current-price">Current {coinName} Price: $ {currentPrice}</Title>
-//         </Col>
-//       </Row>
-//       <Line data={data} options={options} />
-//     </>
-//   );
-// };
-
-
-
-// export default LineChart;
